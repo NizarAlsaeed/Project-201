a010129bd9106555fd000e4104a334944ac85ed0
Nizar Alsaeed <nizar.ayman@gmail.com>
2021-02-13 20:12:16 +0200

    last edits

--- js/app.js
+++ js/app.js
@@ -19,14 +19,12 @@ function question1(){
   switch (q1) {
   case q1 = 'y':
   case q1 = 'yes':
-    alert('You are Right');
-    score++;
-    alert('open the console to know the results of the next quizes');
+    alert('not exactly');
     break;
   case q1 = 'n':
   case q1 = 'no':
-    alert('not exactly');
-    alert('open the console to know the results of the next quizes');
+    alert('You are Right');
+    score++;
     break;
   default:
     alert('You can answer either yes / y or no / n only');
@@ -36,21 +34,15 @@ function question1(){
 function question2(){
   let q2 = prompt('I like Liverbool \n Y or N').toLowerCase();
   if (q2 === 'y' || q2 === 'yes'){
-    console.log('You are Right');
-    score++;
-  }else if(q2 === 'n' || q2 === 'no'){
-    console.log('not exactly');
-  }else{
+    score++;}else if(q2 === 'n' || q2 === 'no');
+  else{
     alert('You can answer either yes / y or no / n only');
   }
 }
 function question3(){
   let q3 = prompt('I\'ve never travelled outside Jordan \n Y or N').toLowerCase();
   if (q3 === 'n' || q3 === 'no'){
-    console.log('You are Right');
-    score++;}
-  else if(q3 === 'y' || q3 === 'yes')
-    console.log('not exactly');
+    score++;}else if(q3 === 'y' || q3 === 'yes');
   else
     alert('You can answer either yes / y or no / n only');
 }
@@ -58,10 +50,7 @@ function question3(){
 function question4(){
   let q4 = prompt('I live in Amman \n Y or N').toLowerCase();
   if (q4 === 'y' || q4 === 'yes'){
-    console.log('You are Right');
-    score++;}
-  else if (q4 === 'n' || q4 === 'no')
-    console.log('not exactly');
+    score++;}else if(q4 === 'n' || q4 === 'no');
   else
     alert('You can answer either yes / y or no / n only');
 }
@@ -69,13 +58,10 @@ function question4(){
 function question5(){
   let q5 = prompt('My laptop brand is hp \n Y or N').toLowerCase();
   if (q5 === 'n' || q5 === 'no'){
-    console.log('You are Right');
-    score++;}
-  else if (q5 === 'y' || q5 === 'yes')
-    console.log('not exactly');
+    score++;}else if(q5 === 'y' || q5 === 'yes');
   else
     alert('You can answer either yes / y or no / n only');
-  alert('It is a lot of fun! \ni want you to guess a number in my head ');
+  alert('i want you to guess a number in my head ');
 }
 
 function question6(){
